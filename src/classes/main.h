b125bad774bf1a8a4cdf03957ac2b381e3459171
Simon Marcel Linden <simon_marcel.linden@smail.th-koeln.de>
2019-01-04 16:33:30 +0100

    Add SRC-Folder

---
+++ src/classes/main.h
@@ -0,0 +1,14 @@
+//
+// Created by Marcel Linden on 04.01.2019.
+//
+
+#ifndef SELECTIONSORT_MAIN_H
+#define SELECTIONSORT_MAIN_H
+
+
+class main {
+
+};
+
+
+#endif //SELECTIONSORT_MAIN_H
---
+++ src/main.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Marcel Linden on 04.01.2019.
+//
+
+#include "classes/main.h"
